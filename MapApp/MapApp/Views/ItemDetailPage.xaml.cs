226051260134e8e815580713da956522c716eef1
anieri <andre.nieri@tecgraf.com.br>
2019-02-19 15:18:09 -0300

    Add project files.

---
+++ MapApp/MapApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MapApp.Models;
+using MapApp.ViewModels;
+
+namespace MapApp.Views {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel) {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage() {
+            InitializeComponent();
+
+            var item = new Item {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
