3a6d98b50b6470340c0a0e86c88f024299462b10
André Nieri <andre.nieri@tecgraf.com.br>
2019-02-22 11:59:48 -0300

    Add comments to TodoReducer

--- ReduxExample/ReduxExample/Reducers/TodoReducer.cs
+++ ReduxExample/ReduxExample/Reducers/TodoReducer.cs
@@ -8,6 +8,9 @@ namespace ReduxExample.Reducers {
     public static class TodoReducer {
         public static TodoState Reduce(TodoState prevState, ITodoAction todoAction) {
             if (todoAction is AddTodoAction addTodoAction) {
+                // In most cases it would make more sense to use an ImmutableList instead of the ObservableCollection
+                // However, it seems that any change in the list would trigger a "render all" in Xamarin.
+                // Thus, we are using the same reference to the ObservableCollection to add the item, returning a new TodoState.
                 prevState.Todos.Add(addTodoAction.Todo);
                 return new TodoState {
                     Todos = prevState.Todos,
